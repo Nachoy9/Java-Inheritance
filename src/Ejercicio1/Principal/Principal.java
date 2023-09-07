6e09fc8cfe3cdbcdbbdfd3c011f7c7ec920aa806
Ignacio Cholaquides <nachoy9@hotmail.com>
2023-09-06 21:18:50 -0300

    Ejercicio 1

--- src/Ejercicio1/Principal/Principal.java
+++ src/Ejercicio1/Principal/Principal.java
@@ -1,3 +1,11 @@
+/*
+Tenemos una clase padre Animal junto con sus 3 clases hijas Perro, Gato, Caballo
+La clase Animal tendrá como atributos el nombre, alimento, edad y raza del Animal.
+Crear un método en la clase Animal a través del cual cada clase hija deberá mostrar luego un
+mensaje por pantalla informando de que se alimenta.
+ */
+
+
 package Ejercicio1.Principal;
 
 import Ejercicio1.Entidades.*;
